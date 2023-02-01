4a29ed971dd855789b4343e2109ddc1f06d18543
Boudjelal Yannick <ndjbouman@gmail.com>
2023-02-01 14:11:26 +0100

    Update HomeLayout.jsx

--- frontend/src/components/HomeLayout.jsx
+++ frontend/src/components/HomeLayout.jsx
@@ -8,7 +8,26 @@ import "../assets/css/Layout.scss";
 export default function HomeLayout() {
   const { pages } = useContext(FolderContext);
   const outlet = useOutlet();
-
+  let location = "";
+  if (
+    !document.cookie.match(import.meta.env.VITE_NAME_COOKIE)
+  ) {
+    localStorage.setItem("location", "/login");
+  }
+  if (
+    localStorage.getItem("location") !== "/" &&
+    localStorage.getItem("location") !== "/login" &&
+    localStorage.getItem("location") !== "/register"
+  ) {
+    location = localStorage.getItem("location");
+  } else {
+    location = "";
+  }
+  // Si connecté redirige vers profile page
+  if (user.email) {
+    return <Navigate to={location || "/user/profile"} replace />;
+  }
+  
   // Creation pages
   let menu = [];
   Object.keys(pages.Home).forEach((item) => {
